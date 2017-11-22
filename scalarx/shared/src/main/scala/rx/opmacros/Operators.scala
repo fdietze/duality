acf9e1498591a903c377274c71f4b6dae3cc01c6
Felix Dietze <felix@beerleader.net>
2017-11-22 18:33:53 +0100

    throw out state-irrelevant operators: flatMap, filter, fold, ...

--- scalarx/shared/src/main/scala/rx/opmacros/Operators.scala
+++ scalarx/shared/src/main/scala/rx/opmacros/Operators.scala
@@ -21,32 +21,6 @@ object Operators {
     (newTree, newOwnerCtx, enclosingCtx)
   }
 
-  def filter[In: c.WeakTypeTag, T: c.WeakTypeTag]
-              (c: whitebox.Context)
-              (f: c.Expr[In => Boolean])
-              (ownerCtx: c.Expr[rx.Ctx.Owner]): c.Expr[Rx.Dynamic[T]] = {
-    import c.universe._
-    val (checkFunc, newCtx, enclosingCtx) = initialize(c)(f.tree, ownerCtx.tree)
-    val initValue = q"${c.prefix}.macroImpls.get(${c.prefix}.node)"
-    resetExpr[Rx.Dynamic[T]](c)(q"""
-      ${c.prefix}.macroImpls.filterImpl($initValue, $checkFunc, $enclosingCtx)
-    """)
-  }
-
-  def fold[T: c.WeakTypeTag, V: c.WeakTypeTag, Wrap[_]]
-          (c: whitebox.Context)
-          (start: c.Expr[Wrap[V]])
-          (f: c.Expr[(Wrap[V], Wrap[T]) => Wrap[V]])
-          (ownerCtx: c.Expr[rx.Ctx.Owner])
-          (implicit w: c.WeakTypeTag[Wrap[_]]): c.Expr[Rx.Dynamic[V]] = {
-
-    import c.universe._
-    val (foldFunc, newCtx, enclosingCtx) = initialize(c)(f.tree, ownerCtx.tree)
-    resetExpr[Rx.Dynamic[V]](c)(q"""
-      ${c.prefix}.macroImpls.foldImpl($start, $foldFunc, $enclosingCtx)
-    """)
-  }
-
   def map[T: c.WeakTypeTag, V: c.WeakTypeTag, Wrap[_]]
          (c: whitebox.Context)
          (f: c.Expr[Wrap[T] => Wrap[V]])
@@ -59,38 +33,6 @@ object Operators {
     Utils.resetExpr[Rx.Dynamic[V]](c)(q"""
       ${c.prefix}.macroImpls.mappedImpl($call, $enclosingCtx)
     """)
-  }
-
-  def flatMap[T: c.WeakTypeTag, V: c.WeakTypeTag, Wrap[_]]
-             (c: whitebox.Context)
-             (f: c.Expr[Wrap[T] => Wrap[Rx[V]]])
-             (ownerCtx: c.Expr[rx.Ctx.Owner])
-             (implicit w: c.WeakTypeTag[Wrap[_]])
-             : c.Expr[Rx.Dynamic[V]] = {
-
-    import c.universe._
-    val (call, newCtx, enclosingCtx) = initialize(c)(f.tree, ownerCtx.tree)
-
-    resetExpr[Rx.Dynamic[V]](c)(q"""
-      ${c.prefix}.macroImpls.flatMappedImpl($call, $enclosingCtx)
-    """)
-  }
-
-
-  def reduce[T: c.WeakTypeTag, Wrap[_]]
-            (c: whitebox.Context)
-            (f: c.Expr[(Wrap[T], Wrap[T]) => Wrap[T]])
-            (ownerCtx: c.Expr[rx.Ctx.Owner])
-            (implicit w: c.WeakTypeTag[Wrap[_]]): c.Expr[Rx.Dynamic[T]] = {
-    import c.universe._
-    val (reduceFunc, newCtx, enclosingCtx) = initialize(c)(f.tree, ownerCtx.tree)
-
-    val initValue = q"${c.prefix}.macroImpls.get(${c.prefix}.node)"
-
-    resetExpr[Rx.Dynamic[T]](c)(q"""
-      ${c.prefix}.macroImpls.reducedImpl($initValue, $reduceFunc, $enclosingCtx)
-    """)
-
   }
 }
 
@@ -108,14 +50,6 @@ trait Operators[T, Wrap[_]]{
   def unwrap[V](t: Wrap[V]): V
   def prefix: Rx[T]
 
-  def flatMappedImpl[V](call: (rx.Ctx.Owner, rx.Ctx.Data) => Wrap[T] => Wrap[Rx[V]],
-                        enclosing: rx.Ctx.Owner): Rx.Dynamic[V] = {
-
-    Rx.build { (ownerCtx, dataCtx) =>
-      prefix.addDownstream(dataCtx)
-      this.unwrap(call(ownerCtx, dataCtx)(this.get(prefix))).apply()(dataCtx)
-    }(enclosing)
-  }
   def mappedImpl[V](call: (rx.Ctx.Owner, rx.Ctx.Data) => Wrap[T] => Wrap[V],
                     enclosing: rx.Ctx.Owner): Rx.Dynamic[V] = {
 
@@ -124,60 +58,4 @@ trait Operators[T, Wrap[_]]{
       this.unwrap(call(ownerCtx, dataCtx)(this.get(prefix)))
     }(enclosing)
   }
-
-  def foldImpl[V](start: Wrap[V],
-                  f: (rx.Ctx.Owner, rx.Ctx.Data) => (Wrap[V], Wrap[T]) => Wrap[V],
-                  enclosing: rx.Ctx.Owner): Rx.Dynamic[V] = {
-
-    var prev: Wrap[V] = start
-    Rx.build { (ownerCtx, dataCtx) =>
-      prefix.addDownstream(dataCtx)
-      prev = f(ownerCtx, dataCtx)(prev, this.get(prefix))
-      this.unwrap(prev)
-    }(enclosing)
-  }
-
-  /**
-    * Split into two to make type-inference work
-    */
-  def reducedImpl(initValue: Wrap[T],
-                  reduceFunc: (rx.Ctx.Owner, rx.Ctx.Data) => (Wrap[T], Wrap[T]) => Wrap[T],
-                  enclosing: rx.Ctx.Owner): Rx.Dynamic[T] = {
-    var init = true
-    def getPrev = this.get(prefix)
-
-    var prev = getPrev
-
-    def next: T = this.unwrap(prev)
-
-    Rx.build { (ownerCtx, dataCtx) =>
-      prefix.addDownstream(dataCtx)
-      if(init) {
-        init = false
-        prev = initValue
-        next
-      } else {
-        prev = reduceFunc(ownerCtx, dataCtx)(prev, getPrev)
-        next
-      }
-    }(enclosing)
-  }
-
-  def filterImpl(start: => Wrap[T],
-                 f: (rx.Ctx.Owner, rx.Ctx.Data) => Wrap[T] => Boolean,
-                 enclosing: rx.Ctx.Owner) = {
-
-    var init = true
-    var prev = this.get(prefix)
-    Rx.build {  (ownerCtx, dataCtx) =>
-      prefix.addDownstream(dataCtx)
-      if(f(ownerCtx, dataCtx)(this.get(prefix)) || init) {
-        init = false
-        prev = start
-      }
-
-      this.unwrap(prev)
-    }(enclosing)
-  }
-
 }
--- scalarx/shared/src/main/scala/rx/package.scala
+++ scalarx/shared/src/main/scala/rx/package.scala
@@ -26,18 +26,6 @@ package object rx {
 
     def map[V](f: Id[T] => Id[V])(implicit ownerCtx: Ctx.Owner): Rx.Dynamic[V] = macro Operators.map[T, V, Id]
 
-    @deprecated("Use RX {} instead", "0.4")
-    def flatMap[V](f: Id[T] => Id[Rx[V]])(implicit ownerCtx: Ctx.Owner): Rx.Dynamic[V] = macro Operators.flatMap[T, V, Id]
-
-    @deprecated("Using Rx for Events is deprecated, use Observables fom e.g. Monix instead", "0.4")
-    def filter(f: Id[T] => Boolean)(implicit ownerCtx: Ctx.Owner): Rx.Dynamic[T] = macro Operators.filter[T,T]
-
-    @deprecated("Using Rx for Events is deprecated, use Observables fom e.g. Monix instead", "0.4")
-    def fold[V](start: Id[V])(f: ((Id[V], Id[T]) => Id[V]))(implicit ownerCtx: Ctx.Owner): Rx.Dynamic[V] = macro Operators.fold[T, V, Id]
-
-    @deprecated("Using Rx for Events is deprecated, use Observables fom e.g. Monix instead", "0.4")
-    def reduce(f: (Id[T], Id[T]) => Id[T])(implicit ownerCtx: Ctx.Owner): Rx.Dynamic[T] = macro Operators.reduce[T, Id]
-
     def foreach(f: T => Unit)(implicit ownerCtx: Ctx.Owner): Obs = node.trigger(f(node.now))
   }
 
@@ -58,18 +46,6 @@ package object rx {
     def macroImpls = new SafeRxOps.Macros(node)
 
     def map[V](f: Try[T] => Try[V])(implicit ownerCtx: Ctx.Owner): Rx.Dynamic[V] = macro Operators.map[T, V, Try]
-
-    @deprecated("Use RX {} instead", "0.4")
-    def flatMap[V](f: Try[T] => Try[Rx[V]])(implicit ownerCtx: Ctx.Owner): Rx.Dynamic[V] = macro Operators.flatMap[T, V, Try]
-
-    @deprecated("Using Rx for Events is deprecated, use Observables fom e.g. Monix instead", "0.4")
-    def filter(f: Try[T] => Boolean)(implicit ownerCtx: Ctx.Owner): Rx.Dynamic[T] = macro Operators.filter[Try[T],T]
-
-    @deprecated("Using Rx for Events is deprecated, use Observables fom e.g. Monix instead", "0.4")
-    def fold[V](start: Try[V])(f: (Try[V], Try[T]) => Try[V])(implicit ownerCtx: Ctx.Owner): Rx.Dynamic[V] = macro Operators.fold[T, V, Try]
-
-    @deprecated("Using Rx for Events is deprecated, use Observables fom e.g. Monix instead", "0.4")
-    def reduce(f: (Try[T], Try[T]) => Try[T])(implicit ownerCtx: Ctx.Owner): Rx.Dynamic[T] = macro Operators.reduce[T, Try]
 
     def foreach(f: T => Unit)(implicit ownerCtx: Ctx.Owner): Obs = node.trigger(node.toTry.foreach(f))
   }
